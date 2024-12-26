99e1d0dab747d9e9b44227eb03d027853667f62b
rachleona <racheltam818@icloud.com>
2024-12-26 02:59:46 +0000

    Basic openvoice perturbation cli

--- src/utils.py
+++ src/utils.py
@@ -1,69 +1,53 @@
 import librosa
-import numpy as np
 import torch
 from faster_whisper import WhisperModel
-from pydub import AudioSegment
 
 # adapted from split_audio_whisper from openvoice
 # processes waveform tensors directly instead of using files
 def split_audio(audio_srs, perturber):
     device = perturber.DEVICE
     sampling_rate = perturber.hps.data.sampling_rate
+    sr_constant = sampling_rate / 1000
 
     if device == "cpu": 
         compute_type="float32"
-        np_type = np.float32
     if device == "cuda": 
         compute_type="float16"
-        np_type = np.float16
 
     model = WhisperModel("medium", device=device, compute_type=compute_type)
 
-    scaled_srs = np.array(audio_srs * (1<<15), dtype=np.int16)
-    audio = AudioSegment(
-        scaled_srs.tobytes(), 
-        frame_rate=sampling_rate,
-        sample_width=scaled_srs.dtype.itemsize, 
-        channels=1
-    )
 
-    max_len = len(audio)
+    max_len = len(audio_srs)
     segments, _ = model.transcribe(audio_srs, beam_size=5, word_timestamps=True)
     segments = list(segments)
 
-    start_time = None
+    start = None
     res = []
 
     for k, seg in enumerate(segments):
         # process with the time
         if k == 0:
-            start_time = max(0, int(seg.start * 1000))
+            start = int(max(0, seg.start * 1000) * sr_constant)
 
-        end_time = min(max_len, int(seg.end * 1000) + 80)
+        end = int(min(max_len, seg.end * 1000 + 80) * sr_constant)
 
         # left 0.08s for each audios
-        audio_seg = audio[start_time : end_time]
+        audio_seg = audio_srs[start : end]
 
         if (len(audio_seg) == 0): continue
-
-        channel_sounds = audio_seg.split_to_mono()
-        samples = [s.get_array_of_samples() for s in channel_sounds]
-
-        audio_seg = np.array(samples).T.astype(np_type)        
-        audio_seg /= np.iinfo(samples[0].typecode).max
-        audio_seg = audio_seg.reshape(-1)
 
         seg_tensor = torch.FloatTensor(audio_seg).to(device)
 
         sr_constant = sampling_rate / 1000
         res.append({
-            'start': int(start_time * sr_constant),
-            'end': int(end_time * sr_constant),
-            'tensor': seg_tensor
+            'start': start,
+            'end': end,
+            'tensor': seg_tensor,
+            'id': k + 1
         })
 
         if k < len(segments) - 1:
-            start_time = max(0, int(segments[k+1].start * 1000 - 80))
+            start = int(max(0, segments[k+1].start * 1000 - 80) * sr_constant)
     
     return res
 
