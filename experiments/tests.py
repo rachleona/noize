18f96a24dc87e7b0071fd69ce205b7ba4112674a
rachleona <racheltam818@icloud.com>
2025-02-06 19:30:16 +0000

    more experiments

--- experiments/tests.py
+++ experiments/tests.py
@@ -11,15 +11,17 @@ pairs = [("sof_00295", "som_01523"),
          ("som_03502", "sof_04415")]
 
 for x, y in pairs:
-    for i in range(5,31,5):
+    for i in range(20,101,10):
         noize(f"clips/{x}.wav", "outputs",
                 output_filename=f"protected_{x}_x{i}.wav",
-                snr_weight=0.025 * i,
-                perturbation_norm_weight=0.25 * i,
+                snr_weight=0.025,
+                perturbation_norm_weight=0.25,
                 frequency_weight=1.5,
                 logs=True,
                 log_file=f"logs/{x}_x{i}.csv",
                 yourtts_weight=i,
+                iterations=500,
+                learning_rate=0.1,
                 cdpam=False,
                 yourtts=False,
                 avc=False,
