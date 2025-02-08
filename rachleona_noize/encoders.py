5a293f99edabdc3a8707257f014b5dda76efa6bc
rachleona <racheltam818@icloud.com>
2025-02-08 21:25:25 +0000

    Added target-based loss

--- rachleona_noize/encoders.py
+++ rachleona_noize/encoders.py
@@ -3,14 +3,14 @@ import sys
 import torch
 import torchaudio
 
-from rachleona_noize.ov_adapted import extract_se
+from rachleona_noize.openvoice.mel_processing import spectrogram_torch
 from rachleona_noize.adaptive_voice_conversion.model import SpeakerEncoder as AvcEncoder
 from rachleona_noize.freevc.speaker_encoder import SpeakerEncoder as FvcEncoder
 from rachleona_noize.yourtts.compute_embeddings import compute_embeddings as ytts_emb
 from TTS.api import TTS
 
 
-class EncoderLoss:
+class ThresholdLoss:
     """
     Unifies interface for interacting with different encoders used in perturbation loss function
 
@@ -55,8 +55,21 @@ class EncoderLoss:
 
         return self.weight * elu(self.threshold - euc_dist)
 
+class TargetLoss:
+    def __init__(self, tgt_emb, f, weight, log_name="", logger=None):
+        self.tgt_emb = tgt_emb
+        self.emb_f = f
+        self.log_name = log_name
+        self.weight = weight
+        self.logger = logger
 
-def generate_openvoice_loss(src_se, perturber):
+    def loss(self, new_tensor):
+        new_emb = self.emb_f(new_tensor)
+        euc_dist = torch.linalg.vector_norm(self.tgt_emb - new_emb)
+
+        return self.weight * euc_dist
+
+def generate_openvoice_loss(src, perturber):
     """
     Generates EncoderLoss instance based on current source clip and perturber config
     Uses OpenVoice tone colour extractor
@@ -70,11 +83,32 @@ def generate_openvoice_loss(src_se, perturber):
 
     Returns
     -------
-    EncoderLoss
+    ThresholdLoss
     """
-    return EncoderLoss(
-        src_se,
-        lambda n: extract_se(n, perturber),
+
+    get_emb = lambda a: ov_extract_se(
+        perturber.model,
+        a,
+        perturber.data_params.filter_length,
+        perturber.data_params.hop_length,
+        perturber.data_params.win_length,
+        perturber.hann_window,
+        perturber.DEVICE)
+    
+    if perturber.target is not None:
+        tgt_se = perturber.target['ov_embed']
+        return TargetLoss(
+            tgt_se,
+            get_emb,
+            perturber.DISTANCE_WEIGHT,
+            "dist",
+            perturber.logger,
+        )
+    
+    src_emb = ov_extract_se(src, perturber)
+    return ThresholdLoss(
+        src_emb,
+        get_emb,
         perturber.DISTANCE_WEIGHT,
         35,
         "dist",
@@ -84,7 +118,7 @@ def generate_openvoice_loss(src_se, perturber):
 
 def generate_yourtts_loss(src, perturber):
     """
-    Generates EncoderLoss instance based on current source clip and perturber config
+    Generates ThresholdLoss instance based on current source clip and perturber config
     Uses coqui ViTS speaker encoder
     (H/ASP speaker recognition model based on ResNet architecture)
 
@@ -97,20 +131,30 @@ def generate_yourtts_loss(src, perturber):
 
     Returns
     -------
-    EncoderLoss
+    ThresholdLoss
     """
     # suppress verbose output from model initialisation
     text_trap = io.StringIO()
     sys.stdout = text_trap
 
-    tts = TTS("tts_models/multilingual/multi-dataset/your_tts").to(perturber.DEVICE)
+    tts = TTS("tts_models/multilingual/multi-dataset/your_tts", gpu=perturber.DEVICE!="cpu")
 
     # restore normal stdout
     sys.stdout = sys.__stdout__
-    model = tts.synthesizer.tts_model.speaker_manager.encoder
-    src_emb = ytts_emb(model, src).detach()
+    model = tts.synthesizer.tts_model.speaker_manager.encoder 
 
-    return EncoderLoss(
+    if perturber.target is not None:
+        tgt_emb = perturber.target['ytts_embed']
+        return TargetLoss(
+            tgt_emb,
+            lambda n: ytts_emb(model, n),
+            perturber.YOURTTS_WEIGHT,
+            "yourtts",
+            perturber.logger,
+        )
+    
+    src_emb = ytts_emb(model, src).detach()
+    return ThresholdLoss(
         src_emb,
         lambda n: ytts_emb(model, n),
         perturber.YOURTTS_WEIGHT,
@@ -122,7 +166,7 @@ def generate_yourtts_loss(src, perturber):
 
 def generate_freevc_loss(src, perturber):
     """
-    Generates EncoderLoss instance based on current source clip and perturber config
+    Generates ThresholdLoss instance based on current source clip and perturber config
     Uses coqui freeVC speaker encoder
 
     Parameters
@@ -134,14 +178,24 @@ def generate_freevc_loss(src, perturber):
 
     Returns
     -------
-    EncoderLoss
+    ThresholdLoss
     """
     model = FvcEncoder(perturber.DEVICE, False)
-    src_emb = model.embed_utterance(src, perturber.data_params.sampling_rate).detach()
-
-    return EncoderLoss(
+    get_emb = lambda n: model.embed_utterance(n, perturber.data_params.sampling_rate)
+    if perturber.target is not None:
+        tgt_emb = perturber.target['fvc_embed']
+        return TargetLoss(
+            tgt_emb,
+            get_emb,
+            perturber.FREEVC_WEIGHT,
+            "freevc",
+            perturber.logger,
+        )
+    
+    src_emb = get_emb(src).detach()
+    return ThresholdLoss(
         src_emb,
-        lambda n: model.embed_utterance(n, perturber.data_params.sampling_rate),
+        get_emb,
         perturber.FREEVC_WEIGHT,
         0.71,
         "freevc",
@@ -151,7 +205,7 @@ def generate_freevc_loss(src, perturber):
 
 def generate_avc_loss(src, perturber):
     """
-    Generates EncoderLoss instance based on current source clip and perturber config
+    Generates ThresholdLoss instance based on current source clip and perturber config
     Uses speaker encoder from adaIN model
 
     Parameters
@@ -163,7 +217,7 @@ def generate_avc_loss(src, perturber):
 
     Returns
     -------
-    EncoderLoss
+    ThresholdLoss
     """
     model = AvcEncoder(**perturber.avc_enc_params).to(perturber.DEVICE)
     model.load_state_dict(
@@ -176,13 +230,24 @@ def generate_avc_loss(src, perturber):
     get_emb = lambda x: model.get_speaker_embeddings(
         x, perturber.avc_hp, perturber.data_params.sampling_rate, perturber.DEVICE
     )
-    src_emb = get_emb(src).detach()
 
-    return EncoderLoss(src_emb, get_emb, perturber.AVC_WEIGHT, 2, "avc", perturber.logger)
+    if perturber.target is not None:
+        tgt_emb = perturber.target['avc_embed']
+        return TargetLoss(
+            tgt_emb,
+            get_emb,
+            perturber.AVC_WEIGHT,
+            "avc",
+            perturber.logger,
+        )
+    
+    src_emb = get_emb(src).detach()
+    return ThresholdLoss(src_emb, get_emb, perturber.AVC_WEIGHT, 2, "avc", perturber.logger)
+
 
 def generate_xtts_loss(src, perturber):
     """
-    Generates EncoderLoss instance based on current source clip and perturber config
+    Generates ThresholdLoss instance based on current source clip and perturber config
     Uses coqui ViTS speaker encoder
     (H/ASP speaker recognition model based on ResNet architecture)
 
@@ -195,28 +260,71 @@ def generate_xtts_loss(src, perturber):
 
     Returns
     -------
-    EncoderLoss
+    ThresholdLoss
     """
     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(perturber.DEVICE)
 
     model = tts.synthesizer.tts_model
-    def get_emb(audio):
-        audio = torch.unsqueeze(audio, 0)
-        audio = audio[:, : perturber.data_params.sampling_rate * 30].to(model.device)
-        
-        audio_16k = torchaudio.functional.resample(audio, perturber.data_params.sampling_rate, 16000)
-        return (
-            model.hifigan_decoder.speaker_encoder.forward(audio_16k.to(model.device), l2_norm=True)
-            .unsqueeze(-1)
-            .to(model.device)
+    get_emb = lambda n: xtts_get_emb(model, n, perturber.data_params.sampling_rate)
+    
+    if perturber.target is not None:
+        tgt_emb = perturber.target['xtts_embed']
+        return TargetLoss(
+            tgt_emb,
+            get_emb,
+            perturber.XTTS_WEIGHT,
+            "xtts",
+            perturber.logger,
         )
+    
     src_emb = get_emb(src).detach()
-
-    return EncoderLoss(
+    return ThresholdLoss(
         src_emb,
         get_emb,
         perturber.YOURTTS_WEIGHT,
         1.2,
         "xtts",
         perturber.logger,
+    )
+
+def ov_extract_se(model, audio_ref_tensor, filter_length, hop_length, win_length, hann_window, device):
+    """
+    Extracts OpenVoice Tone Colour Embeddings from a given audio tensor
+    Adapted from original OpenVoice code to process audio waveform data directly instead of from a file
+
+    Parameters
+    ----------
+    audio_ref_tensor : torch.Tensor
+        the audio waveform data in tensor form
+    perturber : PerturbationGenerator
+        PerturbationGenerator object containing the model and config needed for the extraction
+
+    Returns
+    -------
+    torch.Tensor
+        the tone colour embeddings
+    """
+    y = audio_ref_tensor.unsqueeze(0)
+    y = spectrogram_torch(
+        y,
+        filter_length,
+        hop_length,
+        win_length,
+        hann_window,
+        center=False,
+    ).to(device)
+
+    g = model.ref_enc(y.transpose(1, 2)).unsqueeze(-1)
+
+    return g
+
+def xtts_get_emb(model, audio, sr):
+    audio = torch.unsqueeze(audio, 0)
+    audio = audio[:, : sr * 30].to(model.device)
+    
+    audio_16k = torchaudio.functional.resample(audio, sr, 16000)
+    return (
+        model.hifigan_decoder.speaker_encoder.forward(audio_16k.to(model.device), l2_norm=True)
+        .unsqueeze(-1)
+        .to(model.device)
     )
