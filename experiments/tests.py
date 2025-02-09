a67fa5dda5b0e7b548959cecdbb83ccdd0f60a0f
rachleona <racheltam818@icloud.com>
2025-02-09 18:37:21 +0000

    Combined threshold and target losses

--- experiments/tests.py
+++ experiments/tests.py
@@ -4,30 +4,78 @@ from glob import glob
 
 os.makedirs("logs", exist_ok=True)
 
-pairs = [('sof_00610', 'som_09799'), ('sof_04310', 'som_07505')]
+pairs = [('sof_00610', 'som_01523'), ('sof_04310', 'som_01523'), ('som_09799', 'sof_08886'), ('som_07505', 'sof_08886')]
 for x, y in pairs:
     noize(f"clips2/{x}.wav", "outputs",
-            output_filename=f"protected_{x}_tgt.wav",
+            output_filename=f"protected_{x}_o.wav",
             snr_weight=0.025,
             perturbation_norm_weight=0.25,
             frequency_weight=1.5,
             logs=True,
-            log_file=f"logs/{x}_tgt.csv",
+            log_file=f"logs/{x}_o.csv",
             iterations=500,
             perturbation_level=5,
-            learning_rate=0.1,
             target=y,
-            cdpam=False)
-
-    noize(f"clips2/{y}.wav", "outputs",
-            output_filename=f"protected_{y}_tgt.wav",
+            cdpam=False,
+            yourtts=False,
+            avc=False,
+            freevc=False,
+            xtts=False)
+    
+    noize(f"clips2/{x}.wav", "outputs",
+            output_filename=f"protected_{x}_ox.wav",
             snr_weight=0.025,
             perturbation_norm_weight=0.25,
             frequency_weight=1.5,
             logs=True,
-            log_file=f"logs/{y}_tgt.csv",
+            log_file=f"logs/{x}_ox.csv",
             iterations=500,
             perturbation_level=5,
-            learning_rate=0.1,
-            target=x,
-            cdpam=False)
+            target=y,
+            cdpam=False,
+            yourtts=False,
+            avc=False,
+            freevc=False)
+    
+    noize(f"clips2/{x}.wav", "outputs",
+            output_filename=f"protected_{x}_oa.wav",
+            snr_weight=0.025,
+            perturbation_norm_weight=0.25,
+            frequency_weight=1.5,
+            logs=True,
+            log_file=f"logs/{x}_oa.csv",
+            iterations=500,
+            perturbation_level=5,
+            target=y,
+            cdpam=False,
+            yourtts=False,
+            freevc=False,
+            xtts=False)
+    
+    noize(f"clips2/{x}.wav", "outputs",
+            output_filename=f"protected_{x}_of.wav",
+            snr_weight=0.025,
+            perturbation_norm_weight=0.25,
+            frequency_weight=1.5,
+            logs=True,
+            log_file=f"logs/{x}_of.csv",
+            iterations=500,
+            perturbation_level=5,
+            target=y,
+            cdpam=False,
+            yourtts=False,
+            avc=False,
+            xtts=False)
+    
+    noize(f"clips2/{x}.wav", "outputs",
+            output_filename=f"protected_{x}_oafx.wav",
+            snr_weight=0.025,
+            perturbation_norm_weight=0.25,
+            frequency_weight=1.5,
+            logs=True,
+            log_file=f"logs/{x}_oafx.csv",
+            iterations=500,
+            perturbation_level=5,
+            target=y,
+            cdpam=False,
+            yourtts=False)
