bafeef64f5a6184684d37421cb36bda14ced38e5
rachleona <racheltam818@icloud.com>
2025-02-09 18:37:21 +0000

    Combined threshold and target losses

--- rachleona_noize/encoders.py
+++ rachleona_noize/encoders.py
@@ -10,7 +10,7 @@ from rachleona_noize.yourtts.compute_embeddings import compute_embeddings as ytt
 from TTS.api import TTS
 
 
-class ThresholdLoss:
+class EncoderLoss:
     """
     Unifies interface for interacting with different encoders used in perturbation loss function
 
@@ -37,8 +37,9 @@ class ThresholdLoss:
 
     """
 
-    def __init__(self, src_emb, f, weight, threshold, log_name="", logger=None):
+    def __init__(self, src_emb, tgt_emb, f, weight, threshold, log_name="", logger=None):
         self.src_emb = src_emb
+        self.tgt_emb = tgt_emb
         self.emb_f = f
         self.log_name = log_name
         self.weight = weight
@@ -48,26 +49,14 @@ class ThresholdLoss:
     def loss(self, new_tensor):
         new_emb = self.emb_f(new_tensor)
         euc_dist = torch.linalg.vector_norm(self.src_emb - new_emb)
+        tgt_dist = 0 if self.tgt_emb is None else torch.linalg.vector_norm(self.tgt_emb - new_emb)
         elu = torch.nn.ELU()
 
         if self.logger is not None:
             self.logger.log(self.log_name, euc_dist)
 
-        return self.weight * elu(self.threshold - euc_dist)
-
-class TargetLoss:
-    def __init__(self, tgt_emb, f, weight, log_name="", logger=None):
-        self.tgt_emb = tgt_emb
-        self.emb_f = f
-        self.log_name = log_name
-        self.weight = weight
-        self.logger = logger
-
-    def loss(self, new_tensor):
-        new_emb = self.emb_f(new_tensor)
-        euc_dist = torch.linalg.vector_norm(self.tgt_emb - new_emb)
-
-        return self.weight * euc_dist
+        return self.weight * (elu(self.threshold - euc_dist) + tgt_dist)
+    
 
 def generate_openvoice_loss(src, perturber):
     """
@@ -83,7 +72,7 @@ def generate_openvoice_loss(src, perturber):
 
     Returns
     -------
-    ThresholdLoss
+    EncoderLoss
     """
 
     get_emb = lambda a: ov_extract_se(
@@ -95,19 +84,11 @@ def generate_openvoice_loss(src, perturber):
         perturber.hann_window,
         perturber.DEVICE)
     
-    if perturber.target is not None:
-        tgt_se = perturber.target['ov_embed']
-        return TargetLoss(
-            tgt_se,
-            get_emb,
-            perturber.DISTANCE_WEIGHT,
-            "dist",
-            perturber.logger,
-        )
-    
-    src_emb = ov_extract_se(src, perturber)
-    return ThresholdLoss(
+    src_emb = get_emb(src).detach()
+    tgt_emb = None if perturber.target is None else perturber.target["ov_embed"]
+    return EncoderLoss(
         src_emb,
+        tgt_emb,
         get_emb,
         perturber.DISTANCE_WEIGHT,
         35,
@@ -118,7 +99,7 @@ def generate_openvoice_loss(src, perturber):
 
 def generate_yourtts_loss(src, perturber):
     """
-    Generates ThresholdLoss instance based on current source clip and perturber config
+    Generates EncoderLoss instance based on current source clip and perturber config
     Uses coqui ViTS speaker encoder
     (H/ASP speaker recognition model based on ResNet architecture)
 
@@ -131,7 +112,7 @@ def generate_yourtts_loss(src, perturber):
 
     Returns
     -------
-    ThresholdLoss
+    EncoderLoss
     """
     # suppress verbose output from model initialisation
     text_trap = io.StringIO()
@@ -142,20 +123,12 @@ def generate_yourtts_loss(src, perturber):
     # restore normal stdout
     sys.stdout = sys.__stdout__
     model = tts.synthesizer.tts_model.speaker_manager.encoder 
-
-    if perturber.target is not None:
-        tgt_emb = perturber.target['ytts_embed']
-        return TargetLoss(
-            tgt_emb,
-            lambda n: ytts_emb(model, n),
-            perturber.YOURTTS_WEIGHT,
-            "yourtts",
-            perturber.logger,
-        )
     
     src_emb = ytts_emb(model, src).detach()
-    return ThresholdLoss(
+    tgt_emb = None if perturber.target is None else perturber.target['ytts_embed']
+    return EncoderLoss(
         src_emb,
+        tgt_emb,
         lambda n: ytts_emb(model, n),
         perturber.YOURTTS_WEIGHT,
         0.75,
@@ -166,7 +139,7 @@ def generate_yourtts_loss(src, perturber):
 
 def generate_freevc_loss(src, perturber):
     """
-    Generates ThresholdLoss instance based on current source clip and perturber config
+    Generates EncoderLoss instance based on current source clip and perturber config
     Uses coqui freeVC speaker encoder
 
     Parameters
@@ -178,23 +151,16 @@ def generate_freevc_loss(src, perturber):
 
     Returns
     -------
-    ThresholdLoss
+    EncoderLoss
     """
     model = FvcEncoder(perturber.DEVICE, False)
     get_emb = lambda n: model.embed_utterance(n, perturber.data_params.sampling_rate)
-    if perturber.target is not None:
-        tgt_emb = perturber.target['fvc_embed']
-        return TargetLoss(
-            tgt_emb,
-            get_emb,
-            perturber.FREEVC_WEIGHT,
-            "freevc",
-            perturber.logger,
-        )
     
     src_emb = get_emb(src).detach()
-    return ThresholdLoss(
+    tgt_emb = None if perturber.target is None else perturber.target['fvc_embed']
+    return EncoderLoss(
         src_emb,
+        tgt_emb,
         get_emb,
         perturber.FREEVC_WEIGHT,
         0.71,
@@ -205,7 +171,7 @@ def generate_freevc_loss(src, perturber):
 
 def generate_avc_loss(src, perturber):
     """
-    Generates ThresholdLoss instance based on current source clip and perturber config
+    Generates EncoderLoss instance based on current source clip and perturber config
     Uses speaker encoder from adaIN model
 
     Parameters
@@ -217,7 +183,7 @@ def generate_avc_loss(src, perturber):
 
     Returns
     -------
-    ThresholdLoss
+    EncoderLoss
     """
     model = AvcEncoder(**perturber.avc_enc_params).to(perturber.DEVICE)
     model.load_state_dict(
@@ -230,24 +196,15 @@ def generate_avc_loss(src, perturber):
     get_emb = lambda x: model.get_speaker_embeddings(
         x, perturber.avc_hp, perturber.data_params.sampling_rate, perturber.DEVICE
     )
-
-    if perturber.target is not None:
-        tgt_emb = perturber.target['avc_embed']
-        return TargetLoss(
-            tgt_emb,
-            get_emb,
-            perturber.AVC_WEIGHT,
-            "avc",
-            perturber.logger,
-        )
     
     src_emb = get_emb(src).detach()
-    return ThresholdLoss(src_emb, get_emb, perturber.AVC_WEIGHT, 2, "avc", perturber.logger)
+    tgt_emb = None if perturber.target is None else perturber.target['avc_embed']
+    return EncoderLoss(src_emb, tgt_emb, get_emb, perturber.AVC_WEIGHT, 2, "avc", perturber.logger)
 
 
 def generate_xtts_loss(src, perturber):
     """
-    Generates ThresholdLoss instance based on current source clip and perturber config
+    Generates EncoderLoss instance based on current source clip and perturber config
     Uses coqui ViTS speaker encoder
     (H/ASP speaker recognition model based on ResNet architecture)
 
@@ -260,26 +217,18 @@ def generate_xtts_loss(src, perturber):
 
     Returns
     -------
-    ThresholdLoss
+    EncoderLoss
     """
     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(perturber.DEVICE)
 
     model = tts.synthesizer.tts_model
     get_emb = lambda n: xtts_get_emb(model, n, perturber.data_params.sampling_rate)
     
-    if perturber.target is not None:
-        tgt_emb = perturber.target['xtts_embed']
-        return TargetLoss(
-            tgt_emb,
-            get_emb,
-            perturber.XTTS_WEIGHT,
-            "xtts",
-            perturber.logger,
-        )
-    
     src_emb = get_emb(src).detach()
-    return ThresholdLoss(
+    tgt_emb = None if perturber.target is None else perturber.target['xtts_embed']
+    return EncoderLoss(
         src_emb,
+        tgt_emb,
         get_emb,
         perturber.YOURTTS_WEIGHT,
         1.2,
